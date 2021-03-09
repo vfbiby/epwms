16ad124f6b7d6715971a67043b1988511bbf2617
James Zhang <liulfbaby@gmail.com>
2021-03-09 20:58:22 +0800

    feat: add login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,73 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import tailwind from 'tailwind-rn';
+import {useForm, Controller} from 'react-hook-form';
+
+type LoginFormProps = {
+  username: string;
+  password: string;
+};
 
 export const Login = () => {
+  const {control, handleSubmit, errors} = useForm<LoginFormProps>();
+  const onSubmit = (data: LoginFormProps) =>
+    Alert.alert(data.username.toString());
+
   return (
-    <View>
-      <Text style={tailwind('bg-blue-200 px-4 py-2')}>Login</Text>
+    <View style={tailwind('flex justify-center min-h-full p-2')}>
+      <View style={tailwind('py-2')}>
+        <Text style={tailwind('uppercase')}>用户名: </Text>
+        <Controller
+          control={control}
+          name="username"
+          rules={{required: true}}
+          defaultValue=""
+          render={({onBlur, onChange, value}) => (
+            <TextInput
+              style={tailwind(
+                'px-4 py-3 my-2 border border-red-300 bg-gray-200',
+              )}
+              placeholder="Username"
+              value={value}
+              onBlur={onBlur}
+              onChange={(value) => onChange(value)}
+            />
+          )}
+        />
+        {errors.username && <Text>Username is required!</Text>}
+      </View>
+
+      <View style={tailwind('py-2')}>
+        <Text style={tailwind('uppercase')}>密码: </Text>
+        <Controller
+          control={control}
+          name="password"
+          rules={{required: true}}
+          defaultValue=""
+          render={({onBlur, onChange, value}) => (
+            <TextInput
+              style={tailwind(
+                'px-4 py-3 border my-2 border-red-300 bg-gray-200',
+              )}
+              placeholder="Password"
+              value={value}
+              onBlur={onBlur}
+              onChange={(value) => onChange(value)}
+            />
+          )}
+        />
+        {errors.password && <Text>Password is required!</Text>}
+      </View>
+      <TouchableOpacity style={tailwind('bg-blue-300 py-3')}>
+        <Button onPress={handleSubmit(onSubmit)} title="登录" />
+      </TouchableOpacity>
     </View>
   );
 };
