d9312bc9b1fa26d4664910a29783adf62e547ddc
James Zhang <liulfbaby@gmail.com>
2021-03-09 19:48:20 +0800

    feat: add tailwind-rn

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import tailwind from 'tailwind-rn';
 
 export const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <Text style={tailwind('bg-blue-200 px-4 py-2')}>Login</Text>
     </View>
   );
 };
