b16db3628eeaf91bf05965722d762375ed2139ac
James Zhang <liulfbaby@gmail.com>
2021-03-09 11:55:25 +0800

    feat: add stack navigator

---
+++ src/navigator/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Home} from '../pages/Home';
+import {Detail} from '../pages/Detail';
+
+type RootStackParamList = {
+  Home: undefined;
+  Detail: undefined;
+};
+
+let Stack = createStackNavigator<RootStackParamList>();
+
+export const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ src/pages/Detail.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const Detail = () => {
+  return (
+    <View>
+      <Text>Detail page</Text>
+    </View>
+  );
+};
