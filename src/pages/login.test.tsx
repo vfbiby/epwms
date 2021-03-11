5d0ed169bd69ec9af5e2f46a0f11a704d4731eec
James Zhang <liulfbaby@gmail.com>
2021-03-12 00:44:21 +0800

    feat: add a test for login page

--- src/pages/login.test.tsx
+++ src/pages/login.test.tsx
@@ -2,10 +2,27 @@ import 'react-native';
 import React from 'react';
 import {Login} from './Login';
 import {render} from '@testing-library/react-native';
-import App from '../../App';
+import {AuthContext, Form} from '../context/auth-context';
 
 describe('Login Page', () => {
   it('should show username and password input', () => {
-    render(<App />);
+    const authContext = {
+      user: {
+        id: 1,
+        name: 'bb',
+        email: '3432@qq.com',
+        token: 'valid-token',
+      },
+      login: (form: Form) => Promise.resolve(),
+      logout: () => {},
+    };
+    const {getByPlaceholderText} = render(
+      <AuthContext.Provider value={authContext}>
+        <Login />
+      </AuthContext.Provider>,
+    );
+
+    expect(getByPlaceholderText('Username')).toBeTruthy();
+    expect(getByPlaceholderText('Password')).toBeTruthy();
   });
 });
