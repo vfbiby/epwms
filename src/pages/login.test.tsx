559a1a5a4f7bd15409a75050686f167849fe6771
James Zhang <liulfbaby@gmail.com>
2021-03-12 01:19:03 +0800

    refactor: mock use-auth hook to return data directly

--- src/pages/login.test.tsx
+++ src/pages/login.test.tsx
@@ -2,25 +2,17 @@ import 'react-native';
 import React from 'react';
 import {Login} from './Login';
 import {render} from '@testing-library/react-native';
-import {AuthContext, Form} from '../context/auth-context';
+import {mocked} from 'ts-jest/utils';
+import {useAuth} from '../utils/use-auth';
+
+jest.mock('../utils/use-auth');
 
 describe('Login Page', () => {
   it('should show username and password input', () => {
-    const authContext = {
-      user: {
-        id: 1,
-        name: 'bb',
-        email: '3432@qq.com',
-        token: 'valid-token',
-      },
-      login: (form: Form) => Promise.resolve(),
-      logout: () => {},
-    };
-    const {getByPlaceholderText} = render(
-      <AuthContext.Provider value={authContext}>
-        <Login />
-      </AuthContext.Provider>,
-    );
+    (mocked(useAuth) as jest.Mock).mockReturnValue({
+      login: jest.fn(),
+    });
+    const {getByPlaceholderText} = render(<Login />);
 
     expect(getByPlaceholderText('Username')).toBeTruthy();
     expect(getByPlaceholderText('Password')).toBeTruthy();
